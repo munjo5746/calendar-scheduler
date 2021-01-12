e48d27959607d50a6e198c555572df80cdad263f
Edward Chung <munjo5746@gmail.com>
2021-01-11 21:19:47 -0500

    the header and the columns are adjusted to have space for time slots

--- src/app/CalendarScheduler.tsx
+++ src/app/CalendarScheduler.tsx
@@ -11,11 +11,19 @@ const Calendar: React.FC = () => {
 
     return (
         <div className="calendar-scheduler">
-            <div className="header">
+            <div className="header grid">
+                {<div className="day">{/* for time slots */}</div>}
                 {week.map((d) => (
                     <div className="day">
                         {dateFns.format(d, 'EEEEEE, MMM d')}
                     </div>
+                ))}
+            </div>
+
+            <div className="body grid">
+                {<div className="time-column">{/* time slots */}</div>}
+                {week.map((d, idx) => (
+                    <div className="time-column">{idx}</div>
                 ))}
             </div>
         </div>
